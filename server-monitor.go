db647c551c9a864e7852c34f87465946e04b85ba
jjoos <jan@deelstra.org>
2014-03-22 09:53:57 +0100

    Working sample of gpm.

--- server-monitor.go
+++ server-monitor.go
@@ -1,7 +1,12 @@
 package main
 
-import "os"
+import (
+  "os"
+  "fmt"
+  "github.com/jjoos/process-status-collector"
+)
 
 func main() {
 	os.Stdout.WriteString("Hello World!\n")
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", process_status_collector.Sqrt(2))
 }
